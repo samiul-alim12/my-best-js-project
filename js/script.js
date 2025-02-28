a799889c5f53c1d7582da081ffa43d3e0f67429f
samiul-alim12 <nawabitsamiul@gmail.com>
2025-02-28 15:35:28 +0600

    dynamically changed product done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const ringBtn = document.querySelectorAll('.ring-button')
 for(let btn of ringBtn){
+    let productBase = "images/"
     btn.addEventListener("click",function(event){
+        let color =  event.target.id.replace("-color","");
+        
         let getBtn = event.target;
         for(let checkColor of ringBtn){
             checkColor.classList.remove("border-purple-800")
@@ -9,8 +12,9 @@ for(let btn of ringBtn){
         }
         getBtn.classList.add("border-purple-800")
         getBtn.classList.remove("border-gray-300")
-       
         
+        const productImage = document.getElementById("product-image");
+        productImage.src = productBase + color + '.png'
 
     })
 }
